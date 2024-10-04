95f37481d20a9982ebc698c8c8e3771650a9da36
nyunn2 <jungyun_01@snu.ac.kr>
2024-10-05 00:04:36 +0900

    omok game

--- app/src/main/java/com/example/mytictactoe/GameResultAdapter.kt
+++ app/src/main/java/com/example/mytictactoe/GameResultAdapter.kt
@@ -2,25 +2,22 @@ package com.example.mytictactoe
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mytictactoe.databinding.ItemGameResultBinding
 
-class GameResultAdapter : ListAdapter<GameResult, GameResultAdapter.GameResultViewHolder>(DiffCallback()) {
+class GameResultAdapter(
+    var historyButtons: List<Button>
+) : ListAdapter<GameResult, GameResultAdapter.GameResultViewHolder>(DiffCallback()) {
 
     inner class GameResultViewHolder(private val binding: ItemGameResultBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(gameResult: GameResult) {
             // 보드 상태를 각 TextView에 설정
-            binding.board1.text = gameResult.board[0].toString()
-            binding.board2.text = gameResult.board[1].toString()
-            binding.board3.text = gameResult.board[2].toString()
-            binding.board4.text = gameResult.board[3].toString()
-            binding.board5.text = gameResult.board[4].toString()
-            binding.board6.text = gameResult.board[5].toString()
-            binding.board7.text = gameResult.board[6].toString()
-            binding.board8.text = gameResult.board[7].toString()
-            binding.board9.text = gameResult.board[8].toString()
+            historyButtons.forEachIndexed { index, button ->
+                button.text = gameResult.board[index].toString()
+            }
 
             // 승자 정보 설정
             binding.winnerText.text = when (gameResult.winner) {
--- app/src/main/java/com/example/mytictactoe/MainViewModel.kt
+++ app/src/main/java/com/example/mytictactoe/MainViewModel.kt
@@ -9,7 +9,7 @@ class MainViewModel : ViewModel() {
     private val _currentPlayer = MutableLiveData<Char>('O')
     private val _announcement = MutableLiveData<String>("O의 차례입니다")
     private val _resetButton = MutableLiveData<String>("초기화")
-    private val _board = MutableLiveData<Array<Char>>(Array(9) {' '})
+    private val _board = MutableLiveData<Array<Char>>(Array(25) {' '})
     private val _playing = MutableLiveData<Boolean>(true)
 
     private val _gameResultsList = MutableLiveData<List<GameResult>>()
@@ -28,18 +28,18 @@ class MainViewModel : ViewModel() {
 
     fun checkCurrentState(position: Int) {
         if (_playing.value!! && board.value!![position] == ' ') {
-            val newBoard: Array<Char> = _board.value!!.clone() // 기존 보드를 복사
-            newBoard[position] = _currentPlayer.value!! // 현재 플레이어의 마크를 설정
+            val newBoard: Array<Char> = _board.value!!.clone()
+            newBoard[position] = _currentPlayer.value!!
             _board.value = newBoard
 
-            if (checkVictory(newBoard)) {
+            if (checkVictory(newBoard, _currentPlayer.value!!)) {
                 winnerdata.value = _currentPlayer.value!!
                 _resetButton.value = "한판 더"
                 _announcement.value = "게임 오버"
                 _playing.value = false
             } else {
                 num++
-                if (num == 9) {
+                if (num == 25) {
                     winnerdata.value = ' '
                     _resetButton.value = "한판 더"
                     _announcement.value = "무승부"
@@ -64,19 +64,44 @@ class MainViewModel : ViewModel() {
         _gameResultsList.value = gameResults.toList() // MutableLiveData 업데이트
     }
 
+    fun checkVictory(board: Array<Char>, player: Char): Boolean {
+        val size = 5
+        val winCount = 5 // 승리 조건
 
-    fun checkVictory(board: Array<Char>): Boolean {
-        val victory = arrayOf(
-            arrayOf(0, 1, 2),
-            arrayOf(3, 4, 5),
-            arrayOf(6, 7, 8),
-            arrayOf(0, 3, 6),
-            arrayOf(1, 4, 7),
-            arrayOf(2, 5, 8),
-            arrayOf(0, 4, 8),
-            arrayOf(2, 4, 6)
-        )
-        return victory.any { (a, b, c) -> board[a] == board[b] && board[b] == board[c] && board[a] != ' ' }
+        // 가로
+        for (i in 0 until size) {
+            for (j in 0 until size - winCount + 1) {
+                if ((0 until winCount).all { k -> board[i * size + (j + k)] == player }) {
+                    return true
+                }
+            }
+        }
+        // 세로
+        for (j in 0 until size) {
+            for (i in 0 until size - winCount + 1) {
+                if ((0 until winCount).all { k -> board[(i + k) * size + j] == player }) {
+                    return true
+                }
+            }
+        }
+        // 대각선
+        for (i in 0 until size - winCount + 1) {
+            for (j in 0 until size - winCount + 1) {
+                if ((0 until winCount).all { k -> board[(i + k) * size + (j + k)] == player }) {
+                    return true
+                }
+            }
+        }
+        // 대각선
+        for (i in winCount - 1 until size) {
+            for (j in 0 until size - winCount + 1) {
+                if ((0 until winCount).all { k -> board[(i - k) * size + (j + k)] == player }) {
+                    return true
+                }
+            }
+        }
+
+        return false
     }
 
     fun resetGame() {
@@ -84,7 +109,7 @@ class MainViewModel : ViewModel() {
         _currentPlayer.value = 'O'
         _announcement.value = "O의 차례입니다"
         _resetButton.value = "초기화"
-        _board.value = Array(9) { ' ' }
+        _board.value = Array(25) { ' ' }
         num = 0
     }
 }
